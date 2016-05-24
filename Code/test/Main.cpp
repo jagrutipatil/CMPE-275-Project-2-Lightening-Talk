29d1eda795913e24d6c720279c1f712182d88eed
jagrutipatil <jagrutipatil32@gmail.com>
2016-05-23 22:39:20 -0700

    Running final Code

--- Code/test/Main.cpp
+++ Code/test/Main.cpp
@@ -9,20 +9,44 @@
 #include <ctime>
 #include <chrono>
 #include <thread>
+#include <cmath>
 #include <unistd.h>
 
-#define NUM_THREADS 8
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/uniform_int.hpp>
+#include <boost/random/variate_generator.hpp>
+
+
 #include "../queue/StampingService.h"
 string filePath = "";
-int count = 60000;
+string outFilePath = "./output.txt";
+
+int count = 10000;
 int *arr = new int[count];
 
 Traveler *travelers = new Traveler[count];
 string q1ans[]  = {"India", "USA", "China", "Bangladesh"};
 string felony[]  = {"yes", "no", "minor", "major"};
+const int length = 1000;
+char stamp[length];
+
+float *A = new float[36000];
+float *B = new float[36000];
+float *C = new float[36000];
 
 
 using namespace stampingService;
+
+
+
+extern "C" void stampCalc(const float *A, const float *B, float *C, int numElements)
+{
+	int i = 0;
+	for(i=0;i<numElements;i++)
+	{
+			C[i]  = A[i] * B[i] * pow(A[i]/20,B[i]/20);
+	}
+}
 
 namespace dataGenerator {
 
@@ -45,6 +69,14 @@ namespace dataGenerator {
 		strcat(name, suffix[(rand() % 16)]);
 		name[0] = toupper(name[0]);
 		return;
+	}
+
+	void writeUpateToFile(Traveler *ltravelers) {
+		ofstream myfile;
+		char name[22];
+		myfile.open(outFilePath.c_str(), ios::app | ios::out | ios::in);
+		myfile << ltravelers->toString();
+		myfile.close();
 	}
 
 	void writeToFile() {
@@ -119,7 +151,7 @@ namespace Main {
 	TravelQueue queue;
 	StampingService stmps;
 
-	void batchTravelers() {
+	void loadTravelers() {
 		int byte;
 		string firstname, lastname, visaType, isValidVisa, isStampingDone;
 		std::ifstream infile(filePath.c_str());
@@ -130,7 +162,6 @@ namespace Main {
 			std::istringstream iss(line);
 
 			if (i < count) {
-
 				char * dup = strdup(line.c_str());
 				char * token = strtok(dup, " | ");
 				byte = atoi(token);
@@ -155,7 +186,6 @@ namespace Main {
 					travelers[i].setStampingStatus(false);
 				}
 				i++;
-
 			}
 		}
 
@@ -177,8 +207,29 @@ namespace Main {
 		}
 	}
 
-	bool allQuestionsAnswered(Traveler travelers) {
-		usleep(500);
+	int randomize() {
+		boost::mt19937 gen;
+	    boost::uniform_int<> dist(1, 5);
+	    boost::variate_generator<boost::mt19937&, boost::uniform_int<> > die(gen, dist);
+	    return die();
+	}
+
+	void genVISAStamp(char *s, const int len) {
+	    static const char alphanum[] =
+	        "0123456789"
+	        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	        "abcdefghijklmnopqrstuvwxyz";
+
+	    for (int i = 0; i < len; ++i) {
+	        s[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
+	    }
+	    s[len] = 0;
+	}
+
+
+	bool doWork() {
+		//Calculating Stamp number for the traveler
+		stampCalc(A,B,C,36000/4);
 		return true;
 	}
 
@@ -193,7 +244,7 @@ namespace Main {
 			int byte;
 			string firstname, lastname, visaType, isValidVisa, isStampingDone;
 
-			for(i=omp_get_thread_num(); i < count; i=i + omp_get_num_threads()) {
+			for(i = omp_get_thread_num(); i < count; i = i + omp_get_num_threads()) {
 				infile.clear();
 				if(i < count) {
 					infile.seekg(arr[i]);
@@ -202,34 +253,27 @@ namespace Main {
 				std::istringstream iss(line);
 
 				char * dup = strdup(line.c_str());
+				char * token = strtok(dup, " | ");
+				byte = atoi(token);
 				char delimiter[] = " | ";
 
-					firstname = line.substr(0, line.find(delimiter));
-					line = line.substr(line.find(delimiter)+1, line.length());
-					lastname = line.substr(0, line.find(delimiter));
-					line = line.substr(line.find(delimiter)+1, line.length());
-					visaType = line.substr(0, line.find(delimiter));
-					line = line.substr(line.find(delimiter)+1, line.length());
-					isValidVisa = line.substr(0, line.find(delimiter));
-					line = line.substr(line.find(delimiter)+1, line.length());
-					isStampingDone = line.substr(0, line.find(delimiter));
+				firstname = line.substr(0, line.find(delimiter));
+				line = line.substr(line.find(delimiter)+1, line.length());
+				lastname = line.substr(0, line.find(delimiter));
+				line = line.substr(line.find(delimiter)+1, line.length());
+				visaType = line.substr(0, line.find(delimiter));
+				line = line.substr(line.find(delimiter)+1, line.length());
+				isValidVisa = line.substr(0, line.find(delimiter));
+				line = line.substr(line.find(delimiter)+1, line.length());
+				isStampingDone = line.substr(0, line.find(delimiter));
 
-				if (i < count) {
-					travelers[i].setFirstName(firstname);
-				}
+				travelers[i].setFirstName(firstname);
+				travelers[i].setLastName(lastname);
+				travelers[i].setVisaType(visaType);
 
-				if (i < count) {
-					travelers[i].setLastName(lastname);
-				}
-
-				if (i < count) {
-					travelers[i].setVisaType(visaType);
-				}
-
-				if(i < count && travelers[i].ifValidVisa()) {
-					if (allQuestionsAnswered(travelers[i])) {
-						travelers[i].setStampingStatus(true);
-					}
+				if (i < count && travelers[i].ifValidVisa()) {
+					doWork();
+					travelers[i].setStampingStatus(true);
 				} else if( i < count) {
 					travelers[i].setStampingStatus(false);
 				}
@@ -238,57 +282,74 @@ namespace Main {
 	}
 
 
-	void doSections() {
-		cout << "\nStarted Processing Work";
-		int i = 0;
-		omp_set_num_threads(NUM_THREADS);
-		#pragma omp parallel sections
-		  {
-			if (i < count) {
-				int id = omp_get_thread_num();
-				i++;
-				cout << endl << id;
-			}
-
-		#pragma omp section
-		{
-			#pragma omp parallel
-				{
-					while (i < count) {
-						int id = omp_get_thread_num();
-						cout << endl << "\n Thread:" << id;
-						if (i < count && travelers[i].ifValidVisa()) {
-							travelers[i].setStampingStatus(true);
-						} else {
-							travelers[i].setStampingStatus(false);
-						}
-
-						#pragma omp atomic
-							i++;
-					}
-				}
-		}
-
-		#pragma omp section
-			{
-				#pragma omp parallel
-					{
-						while (i < count) {
-							int id = omp_get_thread_num();
-							cout << endl << "\n Thread in here:" << id;
-							if (i < count && travelers[i].ifValidVisa()) {
-									travelers[i].setStampingStatus(true);
-							} else {
-								travelers[i].setStampingStatus(false);
-							}
-
-						#pragma omp atomic
-							i++;
-				}
-			}
+	void onBoardFlightA(Traveler t) {
+		if (t.ifStampingDone()) {
+			doWork();
 		}
 	}
+
+	void onBoardFlightB(Traveler t) {
+		if (t.ifStampingDone()) {
+			doWork();
+		}
+	}
+
+	void onBoardingParallel() {
+		cout << "\nStarted Traveler onBoarding in Two Sections";
+		int i = 0;
+		int totalOnBoardedPeople = 0;
+		int groupA = 0, groupB = 0;
+
+		#pragma omp parallel sections
+		  {
+				#pragma omp section
+					{
+						for (int i = 0; i < count; i = i + 2) {
+							onBoardFlightA(travelers[i]);
+							#pragma omp atomic
+								groupA++;
+						}
+					}
+
+				#pragma omp section
+					{
+						for (int i = 1; i < count; i = i + 2) {
+							onBoardFlightB(travelers[i]);
+							#pragma omp atomic
+								groupB++;
+						}
+					}
+		  }
+
+		  totalOnBoardedPeople = groupA + groupB;
+		  cout << "\nTotal Travelers: "<< count << endl << "Total onBoarded Travelers:" << totalOnBoardedPeople;
+		  cout << "\n Total onBoarded GroupA Travelers :"<< groupA;
+		  cout << "\n Total Travelers went through final security check:"<< groupB << endl;
   }
+
+	void OnBoardingSequential() {
+		cout << "\nStarted Traveler onBoarding sequentially";
+		int i = 0;
+		int totalOnBoardedPeople = 0;
+		int groupA = 0, groupB = 0;
+
+		for (int i = 0; i < count; i = i + 2) {
+			onBoardFlightA(travelers[i]);
+				groupA++;
+		}
+
+		for (int i = 1; i < count; i = i + 2) {
+			onBoardFlightB(travelers[i]);
+				groupB++;
+		}
+
+		totalOnBoardedPeople = groupA + groupB;
+		cout << "\nTotal Travelers: "<< count << endl << "Total onBoarded Travelers:" << totalOnBoardedPeople;
+		cout << "\n Total Travelers Submitted onBoarding pass:"<< groupA;
+		cout << "\n Total Travelers went through final security check:"<< groupB << endl;
+  }
+
+
 	void progressWork() {
 		cout << "\nStarted Sequential Processing Work"<<endl;
 			int i = 0;
@@ -319,22 +380,18 @@ namespace Main {
 				line = line.substr(line.find(delimiter)+1, line.length());
 				isStampingDone = line.substr(0, line.find(delimiter));
 
-
-
 				travelers[i].setFirstName(firstname);
 				travelers[i].setLastName(lastname);
 				travelers[i].setVisaType(visaType);
 
 				if (travelers[i].ifValidVisa()) {
-					if (allQuestionsAnswered(travelers[i])) {
-						travelers[i].setStampingStatus(true);
-		   		    }
+					doWork();
+					travelers[i].setStampingStatus(true);
 				} else if( i < count) {
 					travelers[i].setStampingStatus(false);
 				}
 			}
 		}
-
 
 
 	void printTravelers() {
@@ -366,6 +423,17 @@ void cleanup() {
   delete travelers;
 }
 
+void initArray() {
+    int j=0;
+    float i=0;
+   	while (j<36000) {
+    	  A[j] = i;
+    	  B[j] = i;
+    	  i++;
+    	  j++;
+    }
+}
+
 
 int main(int argc, char* argv[]) {
 
@@ -374,32 +442,35 @@ int main(int argc, char* argv[]) {
 	if ( argc > 1) {
 		filePath = argv[1];
 		cout<< "\nReading Data From File: " << filePath;
-		cout << "\n************************Parallel Work Statistics******************************";
 	    std::chrono::time_point<std::chrono::system_clock> start, end;
 	    readFirstCharacters();
 
 
-		cout << endl << "Progressing work"<<endl;
+		cout << "\n************************Parallel Work Statistics******************************";
 
 		start = std::chrono::system_clock::now();
+
+		cout << endl << "Started VISA Stamping for Travelers"<<endl;
 		Main:: progressWorkParallel();
+		Main:: onBoardingParallel();
 		end = std::chrono::system_clock::now();
 
 		std::chrono::duration<double> elapsed_seconds = end-start;
 
-		std::cout << "finished computation at " << endl
-	              << "elapsed time: " << elapsed_seconds.count() << "s\n";
+		std::cout << "Finished computation at " << endl
+	              << "Elapsed time: " << elapsed_seconds.count() << "s\n";
 		cout << "\n************************Sequential Work Statistics******************************";
+		cout << endl << "Started VISA Stamping for Travelers"<<endl;
 
-		cout << endl << "Progressing work"<<endl;
 		start = std::chrono::system_clock::now();
 		Main:: progressWork();
+		Main:: OnBoardingSequential();
 		end = std::chrono::system_clock::now();
 
 		elapsed_seconds = end-start;
 
-		std::cout << "finished computation at " << endl
-	              << "elapsed time: " << elapsed_seconds.count() << "s\n";
+		std::cout << "Finished computation at " << endl
+	              << "Elapsed time: " << elapsed_seconds.count() << "s\n";
 
 		cout << "\n******************************************************"<< endl;
 	} else {
